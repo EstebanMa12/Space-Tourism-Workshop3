8b202c654f4b65b9ea1977eb023814addb122100
EstebanMa12 <desteban.maya@udea.edu.co>
2023-10-02 21:33:38 -0500

    Update the styles for the technology page

---
+++ src/templates/Tecnology/index.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./style.css";
+
+export const DesktopTechnologyA = () => {
+  return (
+    <div className="desktop-technology-a">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="group">
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group">
+                <img className="path" alt="Path" src="path.svg" />
+              </div>
+            </div>
+            <div className="div">
+              <div className="rectangle" />
+              <div className="rectangle-2" />
+              <div className="rectangle-3" />
+              <div className="group-2">
+                <div className="group-3">
+                  <div className="text-wrapper">01</div>
+                  <div className="text-wrapper-2">DESTINATION</div>
+                </div>
+                <div className="group-copy">
+                  <div className="text-wrapper">00</div>
+                  <div className="text-wrapper-2">HOME</div>
+                </div>
+                <div className="group-copy-2">
+                  <div className="text-wrapper">02</div>
+                  <div className="text-wrapper-2">CREW</div>
+                </div>
+                <div className="group-copy-3">
+                  <div className="text-wrapper">03</div>
+                  <div className="text-wrapper-2">TECHNOLOGY</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="group-4">
+            <div className="text-wrapper-3">03</div>
+            <div className="text-wrapper-4">SPACE LAUNCH 101</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="overlap-2">
+              <div className="text-wrapper-5">1</div>
+            </div>
+          </div>
+          <div className="group-copy-4">
+            <div className="text-wrapper-6">2</div>
+          </div>
+          <div className="group-copy-5">
+            <div className="text-wrapper-6">3</div>
+          </div>
+          <img className="bitmap" alt="Bitmap" src="image.jpg" />
+          <div className="group-5">
+            <div className="text-wrapper-7">LAUNCH VEHICLE</div>
+            <div className="text-wrapper-8">THE TERMINOLOGY…</div>
+            <p className="a-launch-vehicle-or">
+              A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth&#39;s
+              surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in
+              operation. Standing 150 metres tall, it&#39;s quite an awe-inspiring sight on the launch pad!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
