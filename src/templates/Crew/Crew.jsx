24e26dac984ce7e9a66ecfa3cb9fb9257659eda5
veroa8 <veronica.tangarife1@udea.edu.co>
2023-10-08 19:08:20 -0500

    Gallery changes

--- src/templates/Crew/Crew.jsx
+++ src/templates/Crew/Crew.jsx
@@ -1,197 +1,162 @@
-// import React from 'react';
-// import { Box, Center, Container, Heading, Image, Text } from '@chakra-ui/react';
-// import GoogleFonts from 'react-google-fonts';
 
+import React, { useState } from "react";
+import {
+  Box,
+  Stack,
+  Heading,
+  Text,
+  Container,
+  HStack,
+  Center,
+  Button,
+} from "@chakra-ui/react";
 
-//   const Crew = () => {
-//     const backgroundImageUrl = 'https://res.cloudinary.com/dpc1vrbek/image/upload/v1696612865/Bitmap_tcauwq.jpg'; // Reemplaza 'url_de_tu_imagen.jpg' con la URL de tu imagen de fondo.
-  
-//     const containerStyle = {
-//       backgroundImage: `url(${backgroundImageUrl})`,
-//       backgroundSize: 'cover', // Ajusta el tamaño de la imagen para cubrir todo el contenedor.
-//       backgroundRepeat: 'no-repeat', // Evita que la imagen de fondo se repita.
-//       color: 'white', // Color del texto.
-//       minHeight: '100vh', // Altura mínima para cubrir la pantalla completa.
-//       display: 'flex',
-//       flexDirection: 'column',
-//     };
-//     const numberStyle = {
-//       color: 'gray', // Color gris para el número
-//       marginRight: '8px', // Espacio entre el número y el título principal
-//       fontFamily: 'Barlow Condensed',
-//       fontSize: '16px',
-//       fontWeight: '400',
-//       lineHeight: '19px',
-//       letterSpacing: '2.700000047683716px',
-//       textAlign: 'center',
-//     };
-//     const headingStyle = {
-//       fontFamily: 'Barlow Condensed',
-//       fontSize: '16px',
-//       fontWeight: '400',
-//       lineHeight: '19px',
-//       letterSpacing: '2.700000047683716px',
-//       textAlign: 'center',
-//       padding: '30px',
-      
-//     };
-  
-//   return (
-//     <Container>
-      
-//     <Box style={containerStyle}>
-    
-//       {/* Título */} <GoogleFonts font= "Barlow Condensed"/>
-//       <Box p={4}>
-//       <Heading as="h1" textAlign="center" style={headingStyle}>
-//             <span style={numberStyle}>02</span>
-//             MEET YOUR CREW
-//           </Heading>
-//       </Box>
-
-//       {/* Espacio para imagen */}
-//       <Box>
-//         <Center>
-//           <Image src="https://res.cloudinary.com/dpc1vrbek/image/upload/v1696467752/Group_5_cuiwvq.png" maxW="100%" maxH="100%" objectFit="contain" />
-//         </Center>
-//       </Box>
-
-//       {/* Espacio para escribir texto */}
-//       <Box p={4}>
-//         <Text textAlign="center">Commander Douglas Hurley Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</Text>
-//       </Box>
-//     </Box>
-//     </Container>
-//   );
-// }
-
-// export default Crew;
-
-import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
-
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
-
-const images = [
+const cards = [
   {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+    title: "Design Projects 1",
+    text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+    image:
+      "https://res.cloudinary.com/dpc1vrbek/image/upload/v1696798716/Group_5_vpbdk1.png",
   },
   {
-    label: 'Bird',
-    imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+    title: "Design Projects 2",
+    text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+    image:
+      "https://res.cloudinary.com/dpc1vrbek/image/upload/v1696800415/Group_5_1_jobc9l.png",
   },
   {
-    label: 'Bali, Indonesia',
-    imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
+    title: "Design Projects 3",
+    text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+    image:
+      "https://res.cloudinary.com/dpc1vrbek/image/upload/v1696800415/Group_5_2_c6bhdn.png",
   },
   {
-    label: 'Goč, Serbia',
-    imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-  },
+    title: "Design Projects 4",
+    text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+    image: "https://res.cloudinary.com/dpc1vrbek/image/upload/v1696800415/Group_5_3_utcz5p.png",
+
+  }
 ];
 
-function Crew() {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = images.length;
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+export default function Carousel() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const headingStyle = {
+    fontFamily: 'Barlow Condensed',
+    fontSize: '16px',
+    fontWeight: '400',
+    lineHeight: '19px',
+    letterSpacing: '2.700000047683716px',
+    textAlign: 'center',
+    padding: '30px',
+    
   };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  const numberStyle = {
+    color: 'gray', // Color gris para el número
+    marginRight: '8px', // Espacio entre el número y el título principal
+    fontFamily: 'Barlow Condensed',
+    fontSize: '16px',
+    fontWeight: '400',
+    lineHeight: '19px',
+    letterSpacing: '2.700000047683716px',
+    textAlign: 'center',
   };
-
-  const handleStepChange = (step) => {
-    setActiveStep(step);
+  const backgroundImageUrl = 'https://res.cloudinary.com/dpc1vrbek/image/upload/v1696612865/Bitmap_tcauwq.jpg'; // Reemplaza 'url_de_tu_imagen.jpg' con la URL de tu imagen de fondo.
+  
+    const containerStyle = {
+      backgroundImage: `url(${backgroundImageUrl})`,
+      backgroundSize: 'cover', // Ajusta el tamaño de la imagen para cubrir todo el contenedor.
+      backgroundRepeat: 'no-repeat', // Evita que la imagen de fondo se repita.
+      color: 'white', // Color del texto.
+      minHeight: '100vh', // Altura mínima para cubrir la pantalla completa.
+      display: 'flex',
+      flexDirection: 'column',
+    };
+  const handleDotClick = (index) => {
+    setCurrentIndex(index);
   };
 
   return (
-    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
-      <Paper
-        square
-        elevation={0}
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          height: 50,
-          pl: 2,
-          bgcolor: 'background.default',
-        }}
-      >
-        <Typography>{images[activeStep].label}</Typography>
-      </Paper>
-      <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-      >
-        {images.map((step, index) => (
-          <div key={step.label}>
-            {Math.abs(activeStep - index) <= 2 ? (
-              <Box
-                component="img"
-                sx={{
-                  height: 255,
-                  display: 'block',
-                  maxWidth: 400,
-                  overflow: 'hidden',
-                  width: '100%',
-                }}
-                src={step.imgPath}
-                alt={step.label}
-              />
-            ) : null}
-          </div>
-        ))}
-      </AutoPlaySwipeableViews>
-      <MobileStepper
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      />
-    </Box>
+    <Container style={containerStyle}>
+      <Heading as="h1" textAlign="center" style={headingStyle}>
+        <span style={numberStyle}>02</span>
+        MEET YOUR CREW
+      </Heading>
+      <Box position="relative" height="300px" width="100%" overflow="hidden">
+        {/* Carousel */}
+        <Stack
+          direction="horizontal"
+          spacing={0}
+          width={`${cards.length * 100}%`}
+          transform={`translateX(-${(currentIndex / cards.length) * 100}%)`}
+          transition="transform 0.5s ease-in-out"
+        >
+          {cards.map((card, index) => (
+            <Box
+              key={index}
+              width="100%"
+              height="300px"
+              background={`url(${card.image}) no-repeat`}
+              backgroundPosition={"center"}
+              display="flex"
+              flexDirection={"column"}
+              alignItems="center"
+              justifyContent="center"
+            >
+            </Box>
+          ))}
+        </Stack>
+      </Box>
+     {/* Indicadores */}
+     <Center bottom="20px" width="100%" zIndex={2}>
+        <HStack spacing={2}>
+          {cards.map((_, index) => (
+            <Button
+              key={index}
+              onClick={() => handleDotClick(index)}
+              variant={currentIndex === index ? "teal" : "outline"}
+              size="xs"
+              width={"10px"}
+              height={"10px"}
+              padding={0}
+            ></Button>
+          ))}
+        </HStack>
+      </Center>
+
+      <Box position="relative" height="300px" width="100%" overflow="hidden">
+        {/* Carousel */}
+        <Stack
+          direction="horizontal"
+          spacing={0}
+          width={`${cards.length * 100}%`}
+          transform={`translateX(-${(currentIndex / cards.length) * 100}%)`}
+          transition="transform 0.5s ease-in-out"
+        >
+          {cards.map((card, index) => (
+            <Box
+              key={index}
+              width="100%"
+              height="300px"
+              backgroundPosition={"center"}
+              display="flex"
+              flexDirection={"column"}
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Container size="container.lg" textAlign="center">
+                <Stack spacing={6}>
+                  <Heading fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}>
+                    {card.title}
+                  </Heading>
+                  <Text fontSize={{ base: "md", lg: "lg" }} color="GrayText">
+                    {card.text}
+                  </Text>
+                </Stack>
+              </Container>
+            </Box>
+          ))}
+        </Stack>
+      </Box>
+      </Container>
   );
 }
-
-export default Crew;
