d58011723405d788e678e41ac806a73fec74455f
veroa8 <veronica.tangarife1@udea.edu.co>
2023-10-12 19:12:51 -0500

    version final crew

--- src/templates/Crew/Crew.jsx
+++ src/templates/Crew/Crew.jsx
@@ -1,162 +1,78 @@
 
-import React, { useState } from "react";
-import {
-  Box,
-  Stack,
-  Heading,
-  Text,
-  Container,
-  HStack,
-  Center,
-  Button,
-} from "@chakra-ui/react";
+import {useState, useEffect} from "react";
+import "./Crew.scss";
 
-const cards = [
+const data = [
   {
-    title: "Design Projects 1",
-    text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+    title: "COMMANDER",
+    secondTitle: "DOUGLAS HURLEY",
+    text: "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
     image:
       "https://res.cloudinary.com/dpc1vrbek/image/upload/v1696798716/Group_5_vpbdk1.png",
   },
   {
-    title: "Design Projects 2",
-    text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+    title: "MISSION SPECIALIST",
+    secondTitle: "MARK SHUTTLEWORTH",
+    text: "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
     image:
       "https://res.cloudinary.com/dpc1vrbek/image/upload/v1696800415/Group_5_1_jobc9l.png",
   },
   {
-    title: "Design Projects 3",
-    text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+    title: "PILOT",
+    secondTitle: "VICTOR GLOVER",
+    text: "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.",
     image:
       "https://res.cloudinary.com/dpc1vrbek/image/upload/v1696800415/Group_5_2_c6bhdn.png",
   },
   {
-    title: "Design Projects 4",
-    text: "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+    title: "FLIGHT ENGINEER",
+    secondTitle:"ANOUSHEH ANSARI",
+    text: "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
     image: "https://res.cloudinary.com/dpc1vrbek/image/upload/v1696800415/Group_5_3_utcz5p.png",
-
   }
 ];
 
-export default function Carousel() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const headingStyle = {
-    fontFamily: 'Barlow Condensed',
-    fontSize: '16px',
-    fontWeight: '400',
-    lineHeight: '19px',
-    letterSpacing: '2.700000047683716px',
-    textAlign: 'center',
-    padding: '30px',
-    
-  };
-  const numberStyle = {
-    color: 'gray', // Color gris para el número
-    marginRight: '8px', // Espacio entre el número y el título principal
-    fontFamily: 'Barlow Condensed',
-    fontSize: '16px',
-    fontWeight: '400',
-    lineHeight: '19px',
-    letterSpacing: '2.700000047683716px',
-    textAlign: 'center',
-  };
-  const backgroundImageUrl = 'https://res.cloudinary.com/dpc1vrbek/image/upload/v1696612865/Bitmap_tcauwq.jpg'; // Reemplaza 'url_de_tu_imagen.jpg' con la URL de tu imagen de fondo.
+
+export default function Crew() {
+
+  const [ID, setID] = useState(0);
   
-    const containerStyle = {
-      backgroundImage: `url(${backgroundImageUrl})`,
-      backgroundSize: 'cover', // Ajusta el tamaño de la imagen para cubrir todo el contenedor.
-      backgroundRepeat: 'no-repeat', // Evita que la imagen de fondo se repita.
-      color: 'white', // Color del texto.
-      minHeight: '100vh', // Altura mínima para cubrir la pantalla completa.
-      display: 'flex',
-      flexDirection: 'column',
-    };
-  const handleDotClick = (index) => {
-    setCurrentIndex(index);
-  };
+  const { title, image, text, secondTitle } = data[ID];
+
+  useEffect(() => {
+    const list = document.querySelectorAll('.dots-stepper span');
+    for (let i = 0; i < list.length; i++) {
+      list[i].classList.remove('active');
+    }
+        list[ID].classList.add("active");
+    }, [ID]);
 
   return (
-    <Container style={containerStyle}>
-      <Heading as="h1" textAlign="center" style={headingStyle}>
-        <span style={numberStyle}>02</span>
-        MEET YOUR CREW
-      </Heading>
-      <Box position="relative" height="300px" width="100%" overflow="hidden">
-        {/* Carousel */}
-        <Stack
-          direction="horizontal"
-          spacing={0}
-          width={`${cards.length * 100}%`}
-          transform={`translateX(-${(currentIndex / cards.length) * 100}%)`}
-          transition="transform 0.5s ease-in-out"
-        >
-          {cards.map((card, index) => (
-            <Box
-              key={index}
-              width="100%"
-              height="300px"
-              background={`url(${card.image}) no-repeat`}
-              backgroundPosition={"center"}
-              display="flex"
-              flexDirection={"column"}
-              alignItems="center"
-              justifyContent="center"
-            >
-            </Box>
-          ))}
-        </Stack>
-      </Box>
-     {/* Indicadores */}
-     <Center bottom="20px" width="100%" zIndex={2}>
-        <HStack spacing={2}>
-          {cards.map((_, index) => (
-            <Button
-              key={index}
-              onClick={() => handleDotClick(index)}
-              variant={currentIndex === index ? "teal" : "outline"}
-              size="xs"
-              width={"10px"}
-              height={"10px"}
-              padding={0}
-            ></Button>
-          ))}
-        </HStack>
-      </Center>
+    <div className="container-info">
 
-      <Box position="relative" height="300px" width="100%" overflow="hidden">
-        {/* Carousel */}
-        <Stack
-          direction="horizontal"
-          spacing={0}
-          width={`${cards.length * 100}%`}
-          transform={`translateX(-${(currentIndex / cards.length) * 100}%)`}
-          transition="transform 0.5s ease-in-out"
-        >
-          {cards.map((card, index) => (
-            <Box
-              key={index}
-              width="100%"
-              height="300px"
-              backgroundPosition={"center"}
-              display="flex"
-              flexDirection={"column"}
-              alignItems="center"
-              justifyContent="center"
-            >
-              <Container size="container.lg" textAlign="center">
-                <Stack spacing={6}>
-                  <Heading fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}>
-                    {card.title}
-                  </Heading>
-                  <Text fontSize={{ base: "md", lg: "lg" }} color="GrayText">
-                    {card.text}
-                  </Text>
-                </Stack>
-              </Container>
-            </Box>
-          ))}
-        </Stack>
-      </Box>
-      </Container>
+      <h3 className="mainTitle"><span>02</span>  MEET YOUR CREW</h3>
+
+      <div className="containerImage">
+
+        <div className="characters">
+          <img src={image} alt={title} />
+        </div>
+        <div className="text-and-dots">
+          <div>
+        <h3>{title}</h3>
+          <h2>{secondTitle}</h2>
+          <p>{text}</p>
+          </div>
+          <div className="dots-stepper">
+            <span onClick={() => setID(0)}></span>
+            <span onClick={() => setID(1)}></span>
+            <span onClick={() => setID(2)}></span>
+            <span onClick={() => setID(3)}></span>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   );
 }
